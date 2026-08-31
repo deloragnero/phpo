580916f45b326cf0e4536079769c36887b273f8f
deloragnero <delor.agnero@gmail.com>
2026-08-31 08:03:38 +0000

    Update footer link to admin login page

--- public/inscription.php
+++ public/inscription.php
@@ -213,7 +213,7 @@
 </main>
 
 <footer class="site-footer">
-  <div class="container"><a href="admin/index.php">© 2026 JA SR/PF CI & Communauté des Jeunes Engagés.</a>  Tous droits réservés.</div>
+  <div class="container"><a href="admin/login.php">© 2026 JA SR/PF CI & Communauté des Jeunes Engagés.</a>  Tous droits réservés.</div>
 </footer>
 
 <script src="assets/form.js"></script>
